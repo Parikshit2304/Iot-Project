f0f0e2c7d838d37ca0300db367c2e7e96fb7c00f
Soham Khanna <sohamkhanna08@gmail.com>
2025-03-01 19:40:58 +0530

    Updated features

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Set view engine to EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname,"public")));
 
 // Routes
 app.use(sensorRoutes);
